f16212fa0524c1e66a9e1c1af94058c5d89dba0a
Mark Embling <mark@markembling.info>
2014-01-18 18:37:32 +0000

    Successfully draw dial face

--- gaugicon.js
+++ gaugicon.js
@@ -26,14 +26,15 @@
         min: 0,             // minimum dial value
         max: 100,           // maximum dial value
 
-        bgColour: "#FFFFFF",    // Normal background color
-        colours: [],            // areas of  different bg colour
+        bgColour: "#FFFFFF",        // Normal background color
+        outlineColour: "#000000",   // Outline colour
+        colours: [],                // areas of  different bg colour
 
-        retina: false       // canvas should be 32x32 instead of 16x16 and all dimensions are x2
-        autoApply: true     // automatically apply the new graphic on value set
+        retina: false,      // canvas should be 32x32 instead of 16x16 and all dimensions are x2
+        autoApply: true,    // automatically apply the new graphic on value set
 
-        minAngle: (0 - (Math.PI * 1.29));
-        maxAngle: (Math.PI * 0.29);
+        minAngle: (0 - (Math.PI * 1.29)),
+        maxAngle: (Math.PI * 0.29)
     };
 
     gaugicon.DialGauge.prototype.init = function(options) {
@@ -42,30 +43,30 @@
 
         this._canvas = document.createElement('canvas');
 
-        var size = retina ? 32 : 16;
+        var size = this.options.retina ? 32 : 16;
         this._canvas.width = this._canvas.height = size;
 
-        this._context = _canvas.getContext('2d');
+        this._context = this._canvas.getContext('2d');
     };
 
     /* Update the value and redraw the canvas image */
-    gaugicon.DialGauge.setValue = function(val) {
+    gaugicon.DialGauge.prototype.setValue = function(val) {
         this.value = val;
-        if (autoApply) this.apply();
+        if (this.options.autoApply) this.apply();
     };
 
     /* Update the visible favicon with the current canvas image */
-    gaugicon.DialGauge.apply = function() {
-        var matches = ('link[rel~=icon]');
+    gaugicon.DialGauge.prototype.apply = function() {
+        var matches = $('link[rel~=icon]');
         if (matches.length == 0)
             this._createFaviconElement();
 
         this._draw();
-        $('link[rel~=icon]').attr('href', this.canvas.toDataURL("image/png"));
+        $('link[rel~=icon]').attr('href', this._canvas.toDataURL("image/png"));
     };
 
     /* Creates a favicon link element */
-    gaugicon.DialGauge._createFaviconElement = function() {
+    gaugicon.DialGauge.prototype._createFaviconElement = function() {
         var link = document.createElement('link');
         link.type = 'image/png';
         link.rel = 'shortcut icon';
@@ -73,40 +74,46 @@
     };
 
     /* Selects the correct background colour */
-    gaugicon.DialGauge._chooseBackgroundColour = function() {
+    gaugicon.DialGauge.prototype._chooseBackgroundColour = function() {
         var bg = this.options.bgColour;
         $.each(this.options.colours, function(i,v) {
             if (this.value >= v.from && this.value <= v.to)
                 bg = v.bgColour;
         });
+        return bg;
     };
 
     /* Draw the face of the dial */
-    gauge.DialGauge._drawFace = function() {
+    gaugicon.DialGauge.prototype._drawFace = function() {
         var size = this._canvas.width;
+        var lineWidth = this.options.retina ? 2 : 1.5;
 
-        this._context.fillStyle = _chooseBackgroundColour();
+        this._context.fillStyle = this._chooseBackgroundColour();
+        this._context.strokeStyle = this.options.outlineColour;
+        this._context.lineWidth = lineWidth;
+
         this._context.beginPath();
         this._context.arc(
             size / 2,
             size / 2,
-            (size / 2) - 1
+            (size / 2) - Math.ceil(lineWidth),
             this.options.minAngle,
             this.options.maxAngle,
             false
         );
-        this._context.fill()
+        this._context.fill();
         this._context.stroke();
         this._context.closePath();
     };
 
     /* Draws the image onto the canvas */
-    gaugicon.DialGauge._draw = function() {
+    gaugicon.DialGauge.prototype._draw = function() {
         if (this._context == null) return;  // Canvas appears to be unsupported.
 
         this._context.clearRect(0, 0, this.options.width, this.options.height);
 
         this._drawFace();
+        this._drawNeedle();
     };
 
 
